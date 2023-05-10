b7e339628d4fe7439c42617b73e77d969bc62ddf
leodej <leandrogomes.posto@gmail.com>
2023-05-10 20:41:05 -0300

    Banco Atualizado 10.05.22

--- bancocentral.sql
+++ bancocentral.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 10-Maio-2023 às 19:12
+-- Tempo de geração: 10-Maio-2023 às 23:40
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `animal` (
   `raca` varchar(11) NOT NULL,
   `Obs` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `client` (
   `telefone` varchar(13) NOT NULL,
   `Obs` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -70,14 +70,14 @@ CREATE TABLE IF NOT EXISTS `client` (
 DROP TABLE IF EXISTS `consultas_medicas`;
 CREATE TABLE IF NOT EXISTS `consultas_medicas` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `animal_id` varchar(30) NOT NULL,
-  `veterinario_id` varchar(30) NOT NULL,
+  `animal_id` int NOT NULL,
+  `veterinario_id` int NOT NULL,
   `motivo` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Date_entered` datetime NOT NULL,
   `DateUpdated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `Obs` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`,`animal_id`,`veterinario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -88,15 +88,16 @@ CREATE TABLE IF NOT EXISTS `consultas_medicas` (
 DROP TABLE IF EXISTS `consultas_vet`;
 CREATE TABLE IF NOT EXISTS `consultas_vet` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `veterinario_id` varchar(30) NOT NULL,
-  `animal_id` varchar(30) NOT NULL,
+  `veterinario_id` int NOT NULL,
+  `animal_id` int NOT NULL,
   `Date_entered` datetime NOT NULL,
   `Sintomas` varchar(255) NOT NULL,
   `Diagnostico` varchar(255) NOT NULL,
   `Tratamento` varchar(255) NOT NULL,
   `Tratamento_encerrado` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`,`veterinario_id`,`animal_id`),
+  KEY `veterinario_id` (`veterinario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -107,11 +108,11 @@ CREATE TABLE IF NOT EXISTS `consultas_vet` (
 DROP TABLE IF EXISTS `especies`;
 CREATE TABLE IF NOT EXISTS `especies` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `tipoAnimal_id` varchar(30) NOT NULL,
+  `tipoAnimal_id` int NOT NULL,
   `nome` varchar(30) NOT NULL,
   `descricao` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`,`tipoAnimal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -122,12 +123,12 @@ CREATE TABLE IF NOT EXISTS `especies` (
 DROP TABLE IF EXISTS `exames`;
 CREATE TABLE IF NOT EXISTS `exames` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `consulta_id` varchar(30) NOT NULL,
+  `consulta_id` int NOT NULL,
   `nome` varchar(50) NOT NULL,
   `objetivo` varchar(200) NOT NULL,
   `analisado` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`,`consulta_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -138,11 +139,12 @@ CREATE TABLE IF NOT EXISTS `exames` (
 DROP TABLE IF EXISTS `pagamentos`;
 CREATE TABLE IF NOT EXISTS `pagamentos` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `consulta_id` varchar(30) NOT NULL,
+  `consulta_id` int NOT NULL,
   `valor` float NOT NULL,
   `Status_pagamento` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`,`consulta_id`),
+  KEY `consult_fk_idx` (`consulta_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -153,6 +155,7 @@ CREATE TABLE IF NOT EXISTS `pagamentos` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `CRMV` int NOT NULL,
   `nome` varchar(30) NOT NULL,
   `endereco` varchar(50) NOT NULL,
   `uf` char(2) NOT NULL,
@@ -162,8 +165,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `telefone` varchar(12) NOT NULL,
   `telefone2` varchar(12) NOT NULL,
   `Obs` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`,`CRMV`,`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `pagamentos`
+--
+ALTER TABLE `pagamentos`
+  ADD CONSTRAINT `FK_ID_Consult_Pag` FOREIGN KEY (`consulta_id`) REFERENCES `consultas_vet` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
