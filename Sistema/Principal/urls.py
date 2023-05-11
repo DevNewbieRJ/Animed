9c66cae30136bbbd209503c4a3db7a00201713b7
leodej <leandrogomes.posto@gmail.com>
2023-05-11 15:42:10 -0300

    Atualizaçao

--- Sistema/Principal/urls.py
+++ Sistema/Principal/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from animed.views import FornecedorListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('fornecedores/', FornecedorListView.as_view(), name='fornecedor_list'),
 ]
--- Sistema/animed/views.py
+++ Sistema/animed/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Cliente
+from .models import Veterinario
+from .models import Fornecedor
 
-# Create your views here.
+class ClienteList(ListView):
+    model = Cliente
+    template_name = 'cliente_list.html'
+    context_object_name = 'clientes'
+
+class ClienteDetail(DetailView):
+    model = Cliente
+    template_name = 'cliente_detail.html'
+    context_object_name = 'cliente'
+    
+    
+class VeterinarioListView(ListView):
+    model = Veterinario
+    template_name = 'veterinarios.html'
+    context_object_name = 'veterinarios'
+    
+    
+class FornecedorListView(ListView):
+    model = Fornecedor
+    template_name = 'fornecedor_list.html'
+    context_object_name = 'fornecedores'
+
